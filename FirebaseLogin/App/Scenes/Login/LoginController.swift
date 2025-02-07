013472f502a134544fb53bd50d33dc300785ad5f
Diggo Silva <rdiggosilva@gmail.com>
2025-02-07 09:41:30 -0300

    Implemented authentication/login with firebase

--- FirebaseLogin/App/Scenes/Login/LoginController.swift
+++ FirebaseLogin/App/Scenes/Login/LoginController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginController: UIViewController {
     
@@ -20,6 +21,16 @@ class LoginController: UIViewController {
         super.viewDidLoad()
         setNavBar()
         setDelegatesAndDataSources()
+        navigateToFeedIfUserIsLoggedIn()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        setupLoginButtonWhenTapped(setTitle: "Logar", startAnimating: false)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
     }
     
     private func setNavBar() {
@@ -29,11 +40,68 @@ class LoginController: UIViewController {
     private func setDelegatesAndDataSources() {
         loginView.delegate = self
     }
+    
+    private func navigateToFeedIfUserIsLoggedIn() {
+        let firebaseAuth = Auth.auth()
+        if let user = firebaseAuth.currentUser {
+            if let email = user.email {
+                print(email)
+                let feedVC = FeedController()
+                feedVC.feedView.email = email
+                navigationController?.pushViewController(feedVC, animated: true)
+            }
+        }
+    }
+    
+    func showAlertError(message: String) {
+        let alert = UIAlertController(title: "Erro", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: { action in
+            self.setupLoginButtonWhenTapped(setTitle: "Logar", startAnimating: false)
+        }))
+        present(alert, animated: true)
+    }
+    
+    func setupLoginButtonWhenTapped(setTitle: String = "", startAnimating: Bool = true) {
+        if startAnimating {
+            loginView.loginButton.setTitle(setTitle, for: .normal)
+            loginView.spinner.startAnimating()
+        } else {
+            loginView.loginButton.setTitle(setTitle, for: .normal)
+            loginView.spinner.stopAnimating()
+        }
+    }
 }
 
 extension LoginController: LoginViewDelegate {
     func loginButtonTapped() {
-        print("Tela de LOGIN: Clicou no botão LOGAR")
+        setupLoginButtonWhenTapped()
+        guard let email = loginView.emailTextField.text, !email.trimmingCharacters(in: .whitespaces).isEmpty else {
+            showAlertError(message: "Digite um e-mail válido")
+            loginView.emailTextField.text = ""
+            return
+        }
+        
+        guard let password = loginView.passwordTextField.text, !password.trimmingCharacters(in: .whitespaces).isEmpty else {
+            showAlertError(message: "Digita sua senha")
+            return
+        }
+        
+        let firebaseAuth = Auth.auth()
+        firebaseAuth.signIn(withEmail: email, password: password) { [weak self] authResult, error in
+            guard let self = self else { return }
+            
+            if let error = error {
+                self.showAlertError(message: "Erro ao realizar o login: \(error.localizedDescription)")
+                return
+            } else {
+                let feedVC = FeedController()
+                let firebaseAuth = Auth.auth()
+                if let email = firebaseAuth.currentUser?.email {
+                    feedVC.feedView.email = email
+                    self.navigationController?.pushViewController(feedVC, animated: true)
+                }
+            }
+        }
     }
     
     func signupButtonTapped() {
--- FirebaseLogin/App/Scenes/Signup/SignupController.swift
+++ FirebaseLogin/App/Scenes/Signup/SignupController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class SignupController: UIViewController {
     
@@ -22,6 +23,15 @@ class SignupController: UIViewController {
         setDelegatesAndDataSources()
     }
     
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        setupSignupButtonWhenTapped(setTitle: "Cadastrar", startAnimating: false)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
     private func setNavBar() {
         title = "TELA DE CADASTRO"
         navigationItem.hidesBackButton = true
@@ -30,11 +40,68 @@ class SignupController: UIViewController {
     private func setDelegatesAndDataSources() {
         signupView.delegate = self
     }
+    
+    func showAlertError(message: String) {
+        let alert = UIAlertController(title: "Erro", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: { action in
+            self.setupSignupButtonWhenTapped(setTitle: "Cadastrar", startAnimating: false)
+        }))
+        present(alert, animated: true)
+    }
+    
+    func showAlertSuccess(message: String) {
+        let alert = UIAlertController(title: "Cadastrado com sucesso!", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: { action in
+                do {
+                    try Auth.auth().signOut()
+                    self.navigationController?.popToRootViewController(animated: true)
+                } catch {
+                    self.showAlertError(message: "Erro ao fazer logout: \(error.localizedDescription)")
+            }
+            self.navigationController?.popToRootViewController(animated: true)
+        }))
+        present(alert, animated: true)
+    }
+    
+    func setupSignupButtonWhenTapped(setTitle: String = "", startAnimating: Bool = true) {
+        if startAnimating {
+            signupView.signupButton.setTitle(setTitle, for: .normal)
+            signupView.spinner.startAnimating()
+        } else {
+            signupView.signupButton.setTitle(setTitle, for: .normal)
+            signupView.spinner.stopAnimating()
+        }
+    }
 }
 
 extension SignupController: SignupViewDelegate {
     func signupButtonTapped() {
-        print("Tela de CADASTRO: Clicou no botão CADASTRAR")
+        setupSignupButtonWhenTapped()
+        guard let email = signupView.emailTextField.text, !email.trimmingCharacters(in: .whitespaces).isEmpty else {
+            showAlertError(message: "Digite um e-mail válido")
+            signupView.emailTextField.text = ""
+            return
+        }
+        
+        guard let password = signupView.passwordTextField.text, !password.trimmingCharacters(in: .whitespaces).isEmpty else {
+            showAlertError(message: "Digite uma senha")
+            return
+        }
+        
+        guard let confirmPassword = signupView.confirmPasswordTextField.text, !confirmPassword.trimmingCharacters(in: .whitespaces).isEmpty, confirmPassword == password else {
+            showAlertError(message: "As senhas não coincidem. Digite novamente")
+            signupView.confirmPasswordTextField.text = ""
+            return
+        }
+        
+        let firebaseAuth = Auth.auth()
+        firebaseAuth.createUser(withEmail: email, password: password) { authResult, error in
+            if let error = error {
+                self.showAlertError(message: "Falha ao cadastrar usuário: \(error.localizedDescription)")
+            } else {
+                self.showAlertSuccess(message: "Faça o login para continuar")
+            }
+        }
     }
     
     func loginButtonTapped() {
