37dbb03ba4359212e5cdb51a9b4ffa84a220bf7d
Diggo Silva <rdiggosilva@gmail.com>
2025-03-13 11:54:57 -0300

    XCode configuration

---
+++ FirebaseLoginTests/SignupViewModelTests.swift
@@ -0,0 +1,16 @@
+//
+//  FirebaseLoginTests.swift
+//  FirebaseLoginTests
+//
+//  Created by Diggo Silva on 13/03/25.
+//
+
+import Testing
+
+struct FirebaseLoginTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
